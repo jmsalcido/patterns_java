5368c7689df72478c3a6ae8b8d28161f4bcd81ee
Jose Salcido <jose@otfusion.org>
2013-06-27 11:58:35 -0700

    adding some comments

--- chain_of_responsibility/src/personal/jsalcido/Processor.java
+++ chain_of_responsibility/src/personal/jsalcido/Processor.java
@@ -9,21 +9,49 @@ package personal.jsalcido;
  */
 public abstract class Processor implements IChain {
 
+    // I dont know "why" volatile
+    // I saw this:
+    //
+    // “… the volatile modifier guarantees that any thread that reads a field will see the most recently written value.” - Josh Bloch
+    //
+    // I will take his word.
     protected volatile IChain next;
 
+    /**
+     * set the next chain element
+     * @param next
+     */
     public final void setNext(IChain next) {
         this.next = next;
     }
 
+    /**
+     * process the request
+     * @param request
+     */
     public final void process(Object request) {
         if(this.accept(request)) {
             this.doHandle(request);
         } else {
-            next.process(request);
+            // this may throw an exception right?
+            try {
+                next.process(request);
+            } catch (NullPointerException npe) {
+                System.out.println("No element at the chain could process this request, sorry for your time wasted.");
+            }
         }
     }
 
+    /**
+     * if the request is true the process method will do a callback to the method doHandle
+     * @param request
+     * @return
+     */
     protected abstract boolean accept(Object request);
 
+    /**
+     * handle the request
+     * @param request
+     */
     protected abstract void doHandle(Object request);
 }
